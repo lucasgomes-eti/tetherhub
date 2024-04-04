e43e75a6d79bf10339657cbab10c7ae0d82ca4c9
Lucas Gomes <lucasgomes.exe@gmail.com>
2024-04-04 14:05:21 -0400

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/eti/lucasgomes/tetherhub/Droid.kt
@@ -0,0 +1,12 @@
+package eti.lucasgomes.tetherhub
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun Droid() {
+    Column { Text("I am a Droid") }
+}
---
+++ composeApp/src/commonMain/kotlin/Login.kt
@@ -0,0 +1,12 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun Login() {
+    Column {
+        Text("Login")
+    }
+}
