95613e4f20f4dd00b7e0e26766e511b8f1af82a2
Lucas Gomes <lucasgomes.exe@gmail.com>
2025-08-13 14:53:24 -0400

    fix(server): add mogoentity annotation to PostEntity

--- server/src/main/kotlin/eti/lucasgomes/tetherhub/post/PostEntity.kt
+++ server/src/main/kotlin/eti/lucasgomes/tetherhub/post/PostEntity.kt
@@ -1,10 +1,13 @@
 package eti.lucasgomes.tetherhub.post
 
+import eti.lucasgomes.tetherhub.dsl.MongoEntity
+import eti.lucasgomes.tetherhub.post.PostRepository.Companion.POST_COLLECTION
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import org.bson.types.ObjectId
 
+@MongoEntity(POST_COLLECTION)
 @Serializable
 data class PostEntity(
     @Contextual
